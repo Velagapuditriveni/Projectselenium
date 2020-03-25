3a82e1740fd27daee237c54c21900c3ae2bdf3f2
velagapuditriveni <triveni.selenium@gmail.com>
2020-03-25 10:29:01 +0530

    Jainbookagency

---
+++ JAINBOOKAGENCY_PROJECT/src/test/java/com/testrunner/Jainbook_agencyrunner.java
@@ -0,0 +1,19 @@
+package com.testrunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/main/resources/feature/Testcase.feature",
+		plugin = {"pretty", "html:reports/cucumber-html-report"},
+		tags = {"@TC01_Jainbook,@TC02_Jainbook,@TC03_Jainbook,@TC04_Jainbook"},
+		//tags= {"@Jainbook_agencyrunnerFeature"},
+		glue = {"com.Stepdefinition"},
+		monochrome = true
+		)
+public class Jainbook_agencyrunner {
+
+}
